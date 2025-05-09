2a10c04b85c1e06acad212b3c5809ad9bfd7d842
Eudijuno Scarcela <eudijuno@gmail.com>
2025-05-08 22:25:47 -0300

    First usage of data within html with jinja

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+import pandas as pd
+
+brasileirao_matches = pd.read_csv("data/clean/brasileirao_matches.csv").to_dict(orient="records")
+print(brasileirao_matches)
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_func():
-    return render_template("home.html")
+    return render_template("home.html", matches=brasileirao_matches)
