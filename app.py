c582ccd9147b18cf70fbf31f0562c0c6e71605fb
Eudijuno Scarcela <eudijuno@gmail.com>
2025-05-10 14:56:35 -0300

    Switching from flask to Jinja standalone approach

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from jinja2 import Environment, FileSystemLoader
 import pandas as pd
 
 brasileirao_matches = pd.read_csv("data/clean/brasileirao_matches.csv").to_dict(orient="records")
-print(brasileirao_matches)
 
-app = Flask(__name__)
+templates_dir = "./templates"
+env = Environment(loader=FileSystemLoader(templates_dir))
 
-@app.route("/")
-def home_func():
-    return render_template("home.html", matches=brasileirao_matches)
+def render_home():
+    '''
+    This function aims to render Home static HTML files using Jinja templates
+    and data processed via a notebook.
+    '''
+    home_template = env.get_template("home.html")
+    rendered_home_template = home_template.render(matches=brasileirao_matches)
+    
+    # Save it into the public folder
+    with open("public/home.html", "w") as f:
+        f.write(rendered_home_template)
+
+if __name__ == "__main__":
+    render_home()
